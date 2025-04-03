fc0ed41a79c6c8a535fe24e26a9000371fc4407f
Dhara Pandya <dharapandya2020@gmail.com>
2025-04-03 14:38:06 +0530

    error in requests

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, jsonify, request
 import requests
 import os
 from dotenv import load_dotenv
+import json
 
-load_dotenv()
+
 app=Flask(__name__)
-GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+load_dotenv()
 
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+print("GROQ_API_KEY")
 GROQ_URL="https://api.groq.com/v1/chat/completions"
 
 @app.route("/bot",methods=['POST','GET'])
@@ -21,18 +24,21 @@ def chatbot_response():
             return jsonify({"response":"Please ask a financial question."})
         
         # Sending request to Groq API
-        headers={"Authorization":f"Bearer {GROQ_API_KEY}",
-                 "Content-Type":"application/json"}
-        payload={
-            "model":"llama3-8b-8192",
+        headers={
+            "Authorization": f"Bearer {GROQ_API_KEY}",
+            "Content-Type":"application/json"
+            }
+        payload=json.dumps({
+            "model":"llama-3.3-70b-versatile",
             "messages":[
                 {"role":"system","content":"You are a financial advisor. Answer user queries about saving,investing,and budgeting."},
                 {"role":"user","content":user_message},
             ],
             "temperature":0.7
-        }
+        })
 
-        response=requests.post(GROQ_URL,headers=headers,json=payload)
+        response=requests.post(GROQ_URL,headers=headers,data=payload)
+        print(response.json())
         if response.status_code== 200:
             bot_reply= response.json()["choices"][0]["message"]["content"]
             return jsonify({"response":bot_reply})
@@ -40,6 +46,7 @@ def chatbot_response():
             return jsonify({"repsonse":"Error processing request."})
     except Exception as e:
         return jsonify({"response":f"Error: {str(e)}"})
+ 
     
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
